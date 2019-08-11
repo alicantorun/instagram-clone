33e9948d9b812e1df48300c96f43560d0bfad005
Alican Torun <torun.alican@gmail.com>
2019-08-11 16:03:33 +0200

    backend merged with frontend

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 import "./Post.css";
-
 class Post extends Component {
   render() {
     const nickname = this.props.nickname;
     const avatar = this.props.avatar;
     const image = this.props.image;
     const caption = this.props.caption;
-
     return (
       <article className="Post" ref="Post">
         <header>
@@ -26,11 +24,11 @@ class Post extends Component {
           </div>
         </div>
         <div className="Post-caption">
-          <strong>{nickname}</strong>
-          {caption}
+          <strong>{nickname}</strong> {caption}
         </div>
       </article>
     );
   }
 }
+
 export default Post;
